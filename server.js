973938348636f266a903ea508e444c4afcb25507
Mohamed Asslaoui <mohamedassl@icloud.com>
2026-05-30 15:29:09 +0200

    Update homepagina - deployment test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Cloudsystemen Project",
-    message: "Deze website draait in een Docker-container achter een reverse proxy met HTTPS."
+    message: "Welkom op mijn website voor het vak Cloudsystemen."
   });
 });
 
